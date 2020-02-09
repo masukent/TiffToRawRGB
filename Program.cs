3c993dc05ffe7cb64718467e00e032972ce08aaf
masuk <masukenpo@gmail.com>
2020-02-09 11:01:10 +0900

    update

--- Program.cs
+++ Program.cs
@@ -9,14 +9,52 @@ using System.Windows.Media.Imaging;
 
 namespace TiffToRawRGB
 {
+
+    enum PixelFormat
+    {
+        Default,
+        R16,
+        RGB16,
+    }
+
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        /// <param name="pixelFormat"></param>
+        /// <returns></returns>
+        static int Convert(ref byte[] src, ref byte[] dest, int width, int height, PixelFormat pixelFormat)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    int pos = (y * width) + x;
+                    int stride = 6;
+
+                    // 2byte ずつコピー
+                    for (int d = 0; d < 2; ++d)
+                    {
+                        dest[pos + d] = src[(pos * stride) + d];
+                    }
+
+                }
+            }
+
+
+            return 0;
+        }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="input"></param>
         /// <param name="output"></param>
-        static int Convert(String input, String output)
+        static int Convert(String input, String output, PixelFormat pixelFormat)
         {
 
             try
@@ -33,19 +71,31 @@ namespace TiffToRawRGB
                     if (img.Format == PixelFormats.Gray16)
                     {
                         data = new byte[img.PixelWidth * img.PixelHeight * 2];
-                        stride = (img.PixelWidth * 2);
+                        stride = (img.PixelWidth * 2); // 16bit = 2
+
+                        img.CopyPixels(data, stride, 0);
                     }
                     else if (img.Format == PixelFormats.Rgb48)
                     {
                         data = new byte[img.PixelWidth * img.PixelHeight * 6];
-                        stride = (img.PixelWidth * 6);
+                        stride = (img.PixelWidth * 6); // 48bit = 6
+
+                        img.CopyPixels(data, stride, 0);
+
+                        // Change a rgb to r.
+                        if(pixelFormat == PixelFormat.R16)
+                        {
+                            var newData = new byte[img.PixelWidth * img.PixelHeight * 2];
+
+                            Convert(ref data, ref newData, img.PixelWidth, img.PixelHeight, pixelFormat);
+
+                            data = newData;
+                        }
                     }
 
-                    img.CopyPixels(data, stride, 0);
                     using (var writer = new BinaryWriter(new FileStream(output, FileMode.Create)))
                     {
                         writer.Write(data, 0, data.Length);
-                        Console.WriteLine("[Output] " + output);
                     }
                 }
             }
@@ -65,12 +115,13 @@ namespace TiffToRawRGB
         /// <param name="inputFile"></param>
         /// <param name="outputFile"></param>
         /// <returns></returns>
-        static int ParseArgs(string[] args, ref string inputFile, ref string outputFile)
+        static int ParseArgs(string[] args, ref string inputFile, ref string outputFile, ref PixelFormat pixelFormat)
         {
-            if (args.Length != 4)
+            if (args.Length < 4)
             {
                 Console.WriteLine("[ERROR] Invalid args.");
                 Console.WriteLine("        TiffToRawRGB.exe -i <input> -o <output>");
+                Console.WriteLine("        option -f R16");
                 return 1;
             }
 
@@ -86,6 +137,12 @@ namespace TiffToRawRGB
                     case "-o":
                         ++argIdx;
                         outputFile = args[argIdx];
+                        break;
+
+                    case "-f":
+                        ++argIdx;
+                        var format = args[argIdx].ToUpper();
+                        Enum.TryParse(format, out pixelFormat);
                         break;
 
                     default:
@@ -119,21 +176,26 @@ namespace TiffToRawRGB
         static int Main(string[] args)
         {
             int ret = 0;
+            PixelFormat pixelFormat = PixelFormat.Default;
+
             string inputFile = string.Empty;
             string outputFile = string.Empty;
 
             // Parse commandline arguments.
-            if ( (ret = ParseArgs(args, ref inputFile, ref outputFile) ) != 0 )
+            if ( (ret = ParseArgs(args, ref inputFile, ref outputFile, ref pixelFormat) ) != 0 )
             {
                 return ret;
             }
             
             // Convert tiff file
-            if ( (ret = Convert(inputFile, outputFile)) != 0 )
+            if ( (ret = Convert(inputFile, outputFile, pixelFormat)) != 0 )
             {
                 Console.WriteLine("Failure");
                 return ret;
             }
+
+            Console.WriteLine("[Output] " + outputFile);
+            Console.WriteLine("         Format:" + pixelFormat);
 
             Console.WriteLine("Successfully");
             return 0;
